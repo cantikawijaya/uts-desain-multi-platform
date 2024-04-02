4a74645b6b91ce1a9955c919c2cf4c09eaac79e0
cantikawijaya <cantikawjy@gmail.com>
2024-04-02 18:20:07 +0800

    menambahkan folder assets

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,43 @@ class MyApp extends StatelessWidget {
           title: Text('Sugar Daddy Coffee and Eatery'),
         ),
         body: SingleChildScrollView(
+          Column(
+        children: [
+          Image.asset('assets/img/freedlv.png', width: 80, height: 80),
+          Text('Free Delivery'),
+        ],
+      ),
+          Column(
+        children: [
+          Image.asset('assets/img/dine in.png', width: 80, height: 80),
+          Text('Dine In'),
+        ],
+      ),
+          Column(
+        children: [
+          Image.asset('assets/img/online_order.png', width: 80, height: 80),
+          Text('Online Order'),
+        ],
+      ),
+          Column(
+        children: [
+          Image.asset('assets/img/bestseller.png', width: 80, height: 80),
+          Text('Best Seller'),
+        ],
+      ),
+          Column(
+        children: [
+          Image.asset('assets/img/menusdc.png', width: 80, height: 80),
+          Text('Our Menu'),
+        ],
+      ),
+          Column(
+        children: [
+          Image.asset('assets/img/supp.png', width: 80, height: 80),
+          Text('CS 24/7'),
+        ],
+      ),
+
         ),
       ),
     );
