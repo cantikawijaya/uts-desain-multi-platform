5e772920cc35c0c771e948d87ddaed9be5ef1709
cantikawijaya <cantikawjy@gmail.com>
2024-04-02 17:57:27 +0800

    Membuat struktur dasar dan widget

--- lib/main.dart
+++ lib/main.dart
@@ -7,39 +7,16 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Namer App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+    return MaterialApp(
+      title: 'Sugar Daddy Coffee and Eatery',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Sugar Daddy Coffee and Eatery'),
         ),
-        home: MyHomePage(),
-      ),
-    );
-  }
-}
-
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
-    return Scaffold(
-      body: Column(
-        children: [
-          Text('A random idea:'),
-          Text(appState.current.asLowerCase),
-        ],
+        body: SingleChildScrollView(
+        ),
       ),
     );
   }
