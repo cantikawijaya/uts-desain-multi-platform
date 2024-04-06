6207ac044dba631a66930b49381c447dc18743f5
cantikawijaya <cantikawjy@gmail.com>
2024-04-06 21:10:08 +0800

    Membuat homepage sampai navigasi halaman

--- lib/main.dart
+++ lib/main.dart
@@ -87,6 +87,115 @@ class _MyHomePageState extends State<MyHomePage> {
                 ],
               ),
             ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  _buildMenuItem('Coffee', Icons.coffee, () => navigateToMenuPage('Coffee')),
+                  _buildMenuItem('Tea', Icons.local_drink, () => navigateToMenuPage('Tea')),
+                  _buildMenuItem('Juice', Icons.local_drink, () => navigateToMenuPage('Healthy Juice')),
+                  _buildMenuItem('Breakfast', Icons.fastfood, () => navigateToMenuPage('Breakfast')),
+                  _buildMenuItem('Dessert', Icons.cake, () => navigateToMenuPage('Dessert')),
+                  _buildMenuItem('', Icons.shopping_cart, () => navigateToMenuPage('Your Cart')),
+                ],
+              ),
+            ),
+            // Daftar Menu
+            _buildMenuList(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMenuItem(String title, IconData icon, void Function()? onPressed) {
+    return TextButton.icon(
+      onPressed: onPressed,
+      icon: Icon(icon, size: 32),
+      label: Text(title),
+    );
+  }
+
+  void navigateToMenuPage(String category) {
+  late Widget page;
+  switch (category) {
+    case 'Coffee':
+      page = CoffeeDetailPage(category: category);
+      break;
+    case 'Tea':
+      page = TeaDetailPage(category: category);
+      break;
+    case 'Healthy Juice':
+      page = HealthyJuiceDetailPage(category: category);
+      break;
+    case 'Breakfast':
+      page = Breakfast_Detail_Page(category: category);
+      break;
+    case 'Dessert':
+      page = DessertDetailPage(category: category);
+      break;
+    default:
+      return;
+  }
+  Navigator.push(
+    context,
+    MaterialPageRoute(
+      builder: (context) => page,
+    ),
+  );
+}
+
+  Widget _buildMenuList() {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      itemCount: widget.menuItems.length,
+      itemBuilder: (context, index) {
+        return _buildMenuItemCard(widget.menuItems[index]);
+      },
+    );
+  }
+
+  Widget _buildMenuItemCard(MenuItem menuItem) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          children: [
+            Image.asset(
+              menuItem.imageAssetPath,
+              width: 100,
+              height: 100,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(width: 20),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(menuItem.name),
+                  Text('Rp. ${menuItem.price.toStringAsFixed(2)}'), // Show price with 2 decimal places
+                  Text(menuItem.description),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      ElevatedButton.icon(
+                        onPressed: () {
+                          // Add to cart functionality here
+                          print('${menuItem.name} added to cart!');
+                        },
+                        icon: Icon(Icons.add_shopping_cart),
+                        label: Text('Add to Cart'),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color.fromARGB(255, 255, 255, 255), // Use backgroundColor instead of primary
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
